7742960d6acd2df6698d03712a426a0c064ea7f7
Iqbal Hussain <lincoln@live.in>
2014-06-03 23:31:50 +0530

    deleted everything

--- eureka/__init__.py
+++
@@ -1,5 +0,0 @@
-'''
-Created on 18-May-2014
-
-@author: iqbal
-'''
--- eureka/tests.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on 18-May-2014
-
-@author: iqbal
-'''
-
-from django.test import TestCase
-
-# Create your tests here.
